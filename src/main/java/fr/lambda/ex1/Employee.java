9ec072dcc7ba19395cbf159a9573cadc2da047e7
fredo1975 <fred@autogrif.com>
2025-11-17 21:11:02 +0100

    add new excercice

--- src/main/java/fr/lambda/ex1/Employee.java
+++ src/main/java/fr/lambda/ex1/Employee.java
@@ -1,8 +1,8 @@
 package fr.lambda.ex1;
 
 public class Employee {
-    private String name;
-    private int age;
+    private final String name;
+    private final int age;
 
     public Employee(String name, int age) {
         super();
@@ -12,14 +12,8 @@ public class Employee {
     public String getName() {
         return name;
     }
-    public void setName(String name) {
-        this.name = name;
-    }
     public int getAge() {
         return age;
-    }
-    public void setAge(int age) {
-        this.age = age;
     }
     public String toString()
     {
--- src/main/java/fr/lambda/ex1/Exercice1.java
+++ src/main/java/fr/lambda/ex1/Exercice1.java
@@ -1,18 +1,16 @@
 package fr.lambda.ex1;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Exercice1 {
     public static void main(String[] args){
-        Exercice1 ex = new Exercice1();
+        //Exercice1 ex = new Exercice1();
         Employee emp1 = new Employee("John Doe", 25);
         Employee emp2 = new Employee("Jane Res", 52);
         Employee emp3 = new Employee("Rufus Lowe", 18);
         List<Employee> l = List.of(emp1, emp2,emp3);
         //Given a list of employees, you need to filter all the employee whose age is greater than 20 and print the employee names.(Java 8 APIs only)
-        List<String> res = l.stream().filter(e -> e.getAge() > 20).map(e->e.getName()).toList();
+        List<String> res = l.stream().filter(e -> e.getAge() > 20).map(Employee::getName).toList();
         res.forEach(System.out::println);
 
         //Given the list of employees, count number of employees with age 25?
@@ -20,16 +18,16 @@ public class Exercice1 {
         System.out.println("Number of employees with age 25: "+count);
 
         //Given a list of employee, find maximum age of employee
-        int max = l.stream().max((e1,e2) -> Integer.compare(e1.getAge(), e2.getAge())).get().getAge();
+        int max = l.stream().max(Comparator.comparingInt(Employee::getAge)).get().getAge();
         System.out.println("Maximum age of employee: "+max);
 
         //Given a list of employees, sort all the employee on the basis of age
-        List sorted = l.stream().sorted((c1,c2)-> Integer.compare(c1.getAge(), c2.getAge())).toList();
+        List<Employee> sorted = l.stream().sorted(Comparator.comparingInt(Employee::getAge)).toList();
         System.out.println("Employees sorted by age: ");
         sorted.forEach(System.out::println);
 
         //Join the all employee names with “,”
-        String joinedNames = l.stream().map(e->e.getName()).reduce((a,b) -> a+","+b).get();
+        String joinedNames = l.stream().map(Employee::getName).reduce((a, b) -> a+","+b).get();
         System.out.println("Joined employee names: "+joinedNames);
 
         //Given the list of employee, group them by employee name
@@ -43,7 +41,9 @@ public class Exercice1 {
 
         //Given a list of numbers, square them and filter the numbers which are greater 10000 and then find average of them
         List<Integer> nums = List.of(50, 100, 150, 200, 300);
-        Double average = nums.stream().mapToInt(value -> value*value).filter(v->v>10000).average().getAsDouble();
-        System.out.println("Average of squared numbers greater than 10000: "+average);
+        OptionalDouble average = nums.stream().mapToInt(value -> value*value).filter(v->v>10000).average();
+        if(average.isPresent()){
+            System.out.println("Average of squared numbers greater than 10000: "+average);
+        }
     }
 }
